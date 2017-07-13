260bdf701eb96cdb9838921724ef41478d29231c
xuxiwen <xuxiwen785@163.com>
2017-07-13 08:54:34 +0800

    Initial Commit

---
+++ RecorderDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecorderDemo
+//
+//  Created by xuxiwen on 2017/7/13.
+//  Copyright © 2017年 xuxiwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
